5698d012d0586b9d2ee8eba474b1e3feb10f46a0
Jeffrey Jose <jeffrey.jose@ril.com>
2025-07-21 17:45:39 +0530

    added a project section

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,6 +1,7 @@
 import Hero from "@/components/Hero";
 import Experience from "@/components/Experience";
 import Skills from "@/components/Skills";
+import Projects from "@/components/Projects";
 import Education from "@/components/Education";
 import Contact from "@/components/Contact";
 import { ThemeToggle } from "@/components/theme-toggle";
@@ -12,6 +13,7 @@ const Index = () => {
       <Hero />
       <Experience />
       <Skills />
+      <Projects />
       <Education />
       <Contact />
     </div>
