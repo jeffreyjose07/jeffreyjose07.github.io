51f1d5f7d4f29c0d6980c66b4e5dcfe25546b8a4
jeffreyjose07 <jeffreyjose.k@gmail.com>
2025-11-30 15:19:15 +0530

    fix: implement hash scrolling on initial load

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,3 +1,5 @@
+import { useEffect } from "react";
+import { useLocation } from "react-router-dom";
 import Navigation from "@/components/Navigation";
 import Hero from "@/components/Hero";
 import Experience from "@/components/Experience";
@@ -8,6 +10,19 @@ import Contact from "@/components/Contact";
 import { Helmet } from "react-helmet-async";
 
 const Index = () => {
+  const location = useLocation();
+
+  useEffect(() => {
+    if (location.hash) {
+      const element = document.getElementById(location.hash.substring(1));
+      if (element) {
+        setTimeout(() => {
+          element.scrollIntoView({ behavior: "smooth" });
+        }, 100);
+      }
+    }
+  }, [location]);
+
   return (
     <div className="min-h-screen">
       <Helmet>
